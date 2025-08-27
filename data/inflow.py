c043a809e83f8ab362fcfe713eb433e85a7b71da
Jonathan <jonathanmhtran@gmail.com>
2025-08-27 15:28:56 -0700

    Initial commits

---
+++ data/inflow.py
@@ -0,0 +1,7 @@
+"""
+File: data/inflow.py
+
+This file handles the inflow of data into the system.
+It reads data from yfinance's streaming API and sends it to a Kafka topic.
+"""
+
---
+++ data/scraper.py
@@ -0,0 +1,4 @@
+import duckdb
+import pandas as pd
+
+
