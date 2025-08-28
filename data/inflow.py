c3bb24e53d0469891005bb7ac634279e49bc1dbe
Jonathan <jonathanmhtran@gmail.com>
2025-08-27 17:26:39 -0700

    Restructured project structure (moved /data/ under /src/)

--- data/inflow.py
+++
@@ -1,7 +0,0 @@
-"""
-File: data/inflow.py
-
-This file handles the inflow of data into the system.
-It reads data from yfinance's streaming API and sends it to a Kafka topic.
-"""
-
--- data/scraper.py
+++
@@ -1,4 +0,0 @@
-import duckdb
-import pandas as pd
-
-
