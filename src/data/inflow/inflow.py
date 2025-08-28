2adb5ff2d88462549c5b74ae9b1d12c50c8c32be
Jonathan <jonathanmhtran@gmail.com>
2025-08-27 17:27:44 -0700

    Defined inflow, outflow, Kafka message queue modules for data pipeline

---
+++ src/data/inflow/inflow.py
@@ -0,0 +1,88 @@
+"""
+File: data/inflow.py
+
+This file handles the inflow of data into the system.
+It reads data from yfinance's streaming API and sends it to a Kafka topic.
+"""
+
+import yfinance as yf
+from src.data.mq.mq import KafkaProducerWrapper
+from typing import List, Any, Optional, Dict
+
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+from datetime import datetime
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class TickStatus:
+    """
+    Represents a single stock ticker. Keeps track of its last read timestamp.
+    """
+    def __init__(self, symbol: str):
+        self.symbol = symbol
+        self.last_read: Optional[datetime] = None
+
+class TickerReader:
+    """
+    Reads stock ticker data from yfinance live streams.
+    Provides recovery from the last read timestamp should errors occur
+    """
+    def __init__(self, kafka_producer: KafkaProducerWrapper):
+        self.kafka_producer = kafka_producer
+        self.symbols = self._get_tickers()
+        self.tickers = yf.Tickers(self.symbols)
+        self.last_read = {symbol: TickStatus(symbol) for symbol in self.symbols}
+
+        logger.info(f"Initialized TickerReader with tickers: {self.symbols}")
+    
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+    def start(self):
+        # Start the yfinance streaming API
+        try:
+            logger.info("Starting ticker stream...")
+            self.tickers.live(message_handler=self.message_handler, verbose=False)
+        except Exception as e:
+            logger.error(f"Error in ticker stream: {e}")
+            self.set_last_read()
+
+    def message_handler(self, message: Any):
+        """Handles incoming messages from the yfinance API.
+        """
+        # Update the last read timestamp
+        symbol = message.get('symbol')
+        self.kafka_producer.send_message("finance.tickers", message)
+
+    def _get_tickers(self, file_name: str = "tickers.txt") -> List[str]:
+        """Retrieves a predefined set of tickers to track from tickers.txt
+        """
+        tickers = []
+        with open(file_name, "r") as f:
+            tickers = [line.strip() for line in f.readlines()]
+        return tickers
+
+    def set_last_read(self):
+        """
+        Iterates through the last_read dictionary and writes the last read timestamps to recovery.txt
+        """
+        with open("recovery.txt", "w") as f:
+            for symbol, status in self.last_read.items():
+                f.write(f"{symbol}:{status.last_read}\n")
+
+    async def recover_tickers(self):
+        """
+        Queries yfinance for the lost data given the last read from recovery.txt
+        Uses current time as the end time for the query.
+        """
+        try:
+            current_time = datetime.now()
+            with open("recovery.txt", "r") as f:
+                last_read = f.read().strip()
+            logger.info(f"Recovering tickers from last read: {last_read}")
+            # Query yfinance for the lost data
+            self.tickers.history(period="1d", start=last_read)
+        except Exception as e:
+            logger.error(f"Error recovering tickers: {e}")
---
+++ src/data/mq/mq.py
@@ -0,0 +1,72 @@
+"""
+File: data/mq/mq.py
+
+This file details wrapper classes for Kafka Consumers and Producers.
+"""
+
+from kafka import KafkaConsumer, KafkaProducer
+import json
+
+from typing import List, Any, Dict, Optional
+
+from dotenv import load_dotenv
+import os
+load_dotenv(".env.kafka")
+
+class KafkaProducerWrapper(KafkaProducer):
+    """
+    Wrapper for KafkaProducer
+    """
+    def __init__(self, topic, bootstrap_servers=os.getenv("BOOTSTRAP_SERVER")):
+        self.topic = topic
+        self.bootstrap_servers = bootstrap_servers
+        super().__init__(
+            bootstrap_servers=self.bootstrap_servers,
+            batch_size=32768,       # 32KB batch size
+            linger_ms=500,          # 500ms linger
+            buffer_memory=33554432  # 32MB
+        )
+        
+    def send(self, message) -> None:
+        """Sends a message to the Kafka topic.
+        
+        Args:
+            message (Any): The data to send to the topic
+        """
+        super().send(self.topic, json.dumps(message).encode('utf-8'))
+
+
+class KafkaConsumerWrapper(KafkaConsumer):
+    """
+    Wrapper for KafkaConsumer
+    """
+    def __init__(self, 
+                 topic: str, 
+                 bootstrap_servers=os.getenv("BOOTSTRAP_SERVER"), 
+                 group_id: str|None = None,
+                 max_poll_records: int|None = 100,
+                 session_timeout_ms: int|None = 10000,  # 10 second timeout
+                 heartbeat_interval_ms: int|None = 3000 # 3 second heartbeat
+                 ):
+        self.topic = topic
+        self.bootstrap_servers = bootstrap_servers
+        self.group_id = group_id
+        self.max_poll_records = max_poll_records
+        self.session_timeout_ms = session_timeout_ms
+        self.heartbeat_interval_ms = heartbeat_interval_ms
+        super().__init__(
+            bootstrap_servers=self.bootstrap_servers,
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id=self.group_id,
+            value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+            max_poll_records=self.max_poll_records,
+            session_timeout_ms=self.session_timeout_ms,
+            heartbeat_interval_ms=self.heartbeat_interval_ms
+        )
+
+    def consume(self) -> None:
+        """Consumes messages from the Kafka topic."""
+        for message in self:
+            print(f"Received message: {message.value}")
+            
